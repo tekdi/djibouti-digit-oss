991b118e23c997ff342a9f8affa5c35843ba554f
Akshay Gandhi <akshay.gandhi@tekditechnologies.com>
2025-03-19 14:35:05 +0530

    fix: remove application fee calculation for low risk from bpa calculator service

--- municipal-services/bpa-calculator/src/main/java/org/egov/bpa/calculator/services/CalculationService.java
+++ municipal-services/bpa-calculator/src/main/java/org/egov/bpa/calculator/services/CalculationService.java
@@ -118,10 +118,10 @@ public class CalculationService {
 		if (calulationCriteria.getFeeType().equalsIgnoreCase(BPACalculatorConstants.LOW_RISK_PERMIT_FEE_TYPE)) {
 
 //			 stopping Application fee for lowrisk applicaiton according to BBI-391
-			calulationCriteria.setFeeType(BPACalculatorConstants.MDMS_CALCULATIONTYPE_LOW_APL_FEETYPE);
-			estimatesAndSlabs = getBaseTax(calulationCriteria, requestInfo, mdmsData);
-
-			estimates.addAll(estimatesAndSlabs.getEstimates());
+//			calulationCriteria.setFeeType(BPACalculatorConstants.MDMS_CALCULATIONTYPE_LOW_APL_FEETYPE);
+//			estimatesAndSlabs = getBaseTax(calulationCriteria, requestInfo, mdmsData);
+//
+//			estimates.addAll(estimatesAndSlabs.getEstimates());
 
 			calulationCriteria.setFeeType(BPACalculatorConstants.MDMS_CALCULATIONTYPE_LOW_SANC_FEETYPE);
 			estimatesAndSlabs = getBaseTax(calulationCriteria, requestInfo, mdmsData);
